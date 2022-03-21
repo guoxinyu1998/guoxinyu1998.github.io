78a02a8369e2dfb030c6cac8723975fa2a898157
guoxinyu1998 <984189011@qq.com>
2022-03-21 08:50:10 +0800

    Site updated: 2022-03-21 08:50:06

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ workbox.core.clientsClaim();
 
 // 注册成功后要立即缓存的资源列表
 // 具体缓存列表在gulpfile.js中配置，见下文
-workbox.precaching.precacheAndRoute([{"revision":"70d6629f1dbb9e5118a3aca3bf121bfe","url":"./404.html"},{"revision":"60e56541f441becb27b41e51ebd48595","url":"./index.html"},{"revision":"67ef93b7026979d99da673cfd9fd85de","url":"./js/main.js"},{"revision":"29c7872155eb3e88228c64f7c9e1f005","url":"./css/index.css"}],{
+workbox.precaching.precacheAndRoute([{"revision":"291bccc4f30c22ae2dd8737bc934afa3","url":"./404.html"},{"revision":"0a4f52deb9b3e176228ba9532d3f5158","url":"./index.html"},{"revision":"67ef93b7026979d99da673cfd9fd85de","url":"./js/main.js"},{"revision":"29c7872155eb3e88228c64f7c9e1f005","url":"./css/index.css"}],{
     directoryIndex: null
 });
 
